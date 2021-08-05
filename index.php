59ffa0a2c8ae0380d736d6ace21a2401a68f6b1f
Valkried <celebow@gmail.com>
2021-08-05 22:07:21 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,229 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP PHP POO - Jeu d'échecs</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+            <td style="background-color: #D18B47">
+                Case
+            </td>
+            <td style="background-color: #FFCE9E">
+                Case
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
